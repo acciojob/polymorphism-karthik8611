0731bb39b252f2d8a5acf9c343d6f4864f0345d0
karthik8611 <mkarthik6768@gmail.com>
2024-08-09 13:00:05 +0530

    polymorhism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x+y;
+        };
+
+        public int product(int x, int y, int z) {
+            return x+y+z;
+        };
+        public double product(double x, double y) {
+            return x*y;
+        };
+
+    }
+
+    public static void main(String[] args) {
+        Product p= new Product();
+       int a= p.product(1,2);
+       int b=  p.product(1,2,3);
+       double c=    p.product(2.4,5.3);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
 }
